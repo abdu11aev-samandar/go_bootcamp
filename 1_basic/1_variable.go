d2259371f09a14357a03ec5411d81f65cbe97698
xkas01 <xkas2001@gmail.com>
2023-07-20 10:31:28 +0500

    Declare

--- 1_basic/1_variable.go
+++ 1_basic/1_variable.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+
+	// Variable
 	var age = 10
 	fmt.Println("Age:", age)
 
@@ -16,4 +18,34 @@ func main() {
 	name = "Andrei"
 	name1 := "John"
 	_ = name1
+
+	// Declare
+	car, cost := "Audi", 5000
+	fmt.Println(car, cost)
+	car, year := "BMW", 2018
+	_ = year
+
+	var opened = false
+	opened, file := true, "a.txt"
+
+	_, _ = opened, file
+	var (
+		salary    float64
+		firstName string
+		gender    bool
+	)
+
+	fmt.Println(salary, firstName, gender)
+
+	var a, b, c int
+	fmt.Println(a, b, c)
+
+	var i, j int
+	i, j = 5, 8
+
+	j, i = i, j // swap
+	fmt.Println(i, j)
+
+	sum := 5 + 2.3
+	fmt.Println(sum)
 }
