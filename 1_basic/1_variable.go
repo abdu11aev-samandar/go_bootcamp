274583cd929863bf40ffd533d0656d37c0ef19a5
xkas01 <xkas2001@gmail.com>
2023-07-20 10:17:58 +0500

    Variable

---
+++ 1_basic/1_variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age = 10
+	fmt.Println("Age:", age)
+
+	var name = "Dan"
+	//fmt.Println("Your name is:", name)
+	_ = name
+
+	s := "Learning golang"
+	fmt.Println(s)
+
+	name = "Andrei"
+	name1 := "John"
+	_ = name1
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module bootcamp
+
+go 1.20
