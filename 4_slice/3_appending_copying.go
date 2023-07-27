5849d6d6be0cba2b61f4badb9f359ce4c6c1f9c5
samandar <xkas2001@gmail.com>
2023-07-27 11:43:01 +0500

    Appending to a Slice. Copying slices

---
+++ 4_slice/3_appending_copying.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{2, 3}
+
+	numbers = append(numbers, 10)
+	fmt.Println(numbers)
+
+	numbers = append(numbers, 20, 30, 40)
+	fmt.Println(numbers)
+
+	n := []int{100, 200}
+	numbers = append(numbers, n...)
+	fmt.Println(numbers)
+
+	src := []int{10, 20, 30}
+	dst := make([]int, len(src))
+	nn := copy(dst, src)
+	fmt.Println(src, dst, nn)
+}
