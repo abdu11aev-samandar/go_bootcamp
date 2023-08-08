85628a6ec5f0748881794c19f922f0ff08c3dccc
samandar <xkas2001@gmail.com>
2023-08-08 11:22:00 +0500

    Goroutines and channels

---
+++ 12_concurrency/7_goroutine_channel.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func factorial(n int, c chan int) {
+	f := 1
+	for i := 2; i < n; i++ {
+		f *= i
+	}
+	c <- f
+}
+
+func main() {
+	ch := make(chan int)
+	defer close(ch)
+
+	go factorial(5, ch)
+
+	f := <-ch
+	fmt.Println(f)
+
+	for i := 1; i < 20; i++ {
+		go factorial(i, ch)
+		f := <-ch
+		fmt.Println(f)
+	}
+}
