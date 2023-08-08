354d405e84f3a49857f34542afbfb36dec9e5939
samandar <xkas2001@gmail.com>
2023-08-08 11:00:50 +0500

    Mutexes

--- 12_concurrency/5_data_race.go
+++ 12_concurrency/5_data_race.go
@@ -14,16 +14,26 @@ func main() {
 
 	var n int = 0
 
+	//1.
+	var m sync.Mutex
+
 	for i := 0; i < gr; i++ {
 		go func() {
 			time.Sleep(time.Second / 10)
+			//2.
+			m.Lock()
 			n++
+			m.Unlock()
 			wg.Done()
 		}()
 
 		go func() {
 			time.Sleep(time.Second / 10)
+			//2.
+			m.Lock()
+			defer m.Unlock()
 			n--
+			//m.Unlock()
 			wg.Done()
 		}()
 	}
